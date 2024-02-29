953e37c32b1bed01aa9a29eb32cf93ce3be68a1c
Greg <gregoire@saugnac.me>
2024-02-29 13:38:10 +0000

    Update pendulum_motion.py

--- pendulum_motion.py
+++ pendulum_motion.py
@@ -6,3 +6,27 @@ width, height = 300, 300
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("pendulum motion")
 clock = pygame.time.Clock()
+
+# COLOURS
+BG_COLOUR = (255, 250, 220)
+
+def draw_surface() -> None:
+    
+    win.fill(BG_COLOUR)
+    pygame.display.update()
+    
+def main() -> None:
+    
+    while True:
+        
+        clock.tick(60)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        
+        draw_surface()
+
+if __name__ == '__main__':
+    main()
