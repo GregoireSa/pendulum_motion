f72144c9a1067bc91ebd88005e1e4491db3d0ef3
Greg <gregoire@saugnac.me>
2024-03-05 10:58:57 +0000

    Pivot, angles and links between particles

--- pendulum_motion.py
+++ pendulum_motion.py
@@ -1,6 +1,8 @@
 import pygame
 import math
 import basicUI
+import random
+from time import time
 
 pygame.init()
 
@@ -20,27 +22,55 @@ PARTICLE_COLOUR = (255, 0, 0)
 
 class Particle:
 
-    def __init__(self, mass_slider, radius_slider, particles, center, colour=PARTICLE_COLOUR) -> None:
+    def __init__(self, mass_slider, radius_slider, particles, angle, force=0, colour=PARTICLE_COLOUR) -> None:
 
         if particles:
-            self.pivot = self.particles[-1].center
+            self.first = False
+            self.pivot = particles[-1].center
         else:
+            self.first = True
             self.pivot = (sim_width // 2, 100)
+            
         particles.append(self)
+        self.index = len(particles) - 1
         self.particles = particles
 
-        self.mass = mass_slider.value
+        self.angle = angle
+        self.force = force
+
         self.mass_slider = mass_slider
-        self.radius = radius_slider.value
         self.radius_slider = radius_slider
         
         self.colour = colour
-        self.center = center
+        self.center = (self.pivot[0] + (self.radius_slider.value*200*math.cos(self.angle)),
+                       self.pivot[1] + (self.radius_slider.value*200*math.sin(self.angle)))
+        
     
-    def draw(self, surface) -> None:
+    def draw_line(self, surface) -> None:
+        
+        pygame.draw.line(surface, (0, 0, 0), self.pivot, self.center, width=2)
+    
+    def draw_particle(self, surface) -> None:
 
         pygame.draw.circle(surface, self.colour, self.center, (self.mass_slider.value * 40) + 10)
 
+    def get_angular_velocity(self) -> None:
+        
+        pass
+    
+    def get_centre(self) -> None:
+        
+        pass
+
+    def update(self) -> None:
+        
+        self.center = (self.pivot[0] + (self.radius_slider.value*200*math.cos(self.angle)),
+                       self.pivot[1] + (self.radius_slider.value*200*math.sin(self.angle)))
+
+        if not self.first:
+            self.pivot = self.particles[self.index - 1].center
+        
+    
 text_margin = 50
 
 sliders = {}
@@ -56,7 +86,8 @@ mass2 = new_slider("Mass 2:", (text_margin + sim_width + ui_width // 2, 200))
 radius2 = new_slider("Radius 2:", (text_margin + sim_width + ui_width // 2, 250))
 
 particles = []
-particles.append(Particle(mass1, radius1, particles, (sim_width // 2, 300)))
+Particle(mass1, radius1, particles, 0.1)
+Particle(mass2, radius2, particles, 0.9)
 
 def draw_ui() -> None:
 
@@ -73,7 +104,9 @@ def draw_surface() -> None:
     win.fill(BG_COLOUR)
 
     for ptc in particles:
-        ptc.draw(win)
+        ptc.draw_line(win)
+    for ptc in particles:
+        ptc.draw_particle(win)
 
     draw_ui()
 
@@ -85,13 +118,36 @@ def main() -> None:
         
         clock.tick(60)
         
+        keys = pygame.key.get_pressed()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.KEYDOWN:
+                
+                if pygame.key.get_pressed()[pygame.K_EQUALS]:
+                    particles[-1].angle += 0.1
+                if pygame.key.get_pressed()[pygame.K_MINUS]:
+                    particles[-1].angle -= 0.1
+        
+        if particles:
+            ptc = particles[0]
+            diff = 2
+            if keys[pygame.K_RIGHT]:
+                ptc.pivot = (ptc.pivot[0] + diff, ptc.pivot[1])
+            if keys[pygame.K_LEFT]:
+                ptc.pivot = (ptc.pivot[0] - diff, ptc.pivot[1])
+            if keys[pygame.K_UP]:
+                ptc.pivot = (ptc.pivot[0], ptc.pivot[1] - diff)
+            if keys[pygame.K_DOWN]:
+                ptc.pivot = (ptc.pivot[0], ptc.pivot[1] + diff)
         
         for slider_id in sliders:
             sliders[slider_id].update()
+            
+        for ptc in particles:
+            ptc.update()
 
         draw_surface()
 
